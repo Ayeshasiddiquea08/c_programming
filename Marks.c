b9c4626c4a06369e678fac90abe48dabe3af2fb0
Ayeshasiddiquea08 <83860266+Ayeshasiddiquea08@users.noreply.github.com>
2021-05-08 13:33:00 +0530

    Create Marks.c

---
+++ Marks.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int physics,chemistry,maths;
+	printf("Enter physics marks");
+	scanf("%d,& physics");
+	printf("Enter chemistry marks");
+	scanf("%d,& chemistry");
+	printf("Enter maths marks");
+	scanf("%d,& maths");
+	return 0;
+	
+}
